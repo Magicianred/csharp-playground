03ff74b85436b5d2ebc2bfeb43d27e5a1762413d
Massimiliano Balestrieri <massimiliano.balestrieri@deltatre.it>
2020-08-24 16:28:11 +0200

    events + delegates

---
+++ CSharpPlayground/DelegateTests.cs
@@ -0,0 +1,92 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using System;
+
+namespace CSharpPlayground
+{
+    delegate string MyDelegate(string s);
+    delegate int MyMathDelegate(int s);
+
+    public class DelegateTests
+    {
+        [Test]
+        public void DelegateTest()
+        {
+            MyDelegate d = Print;
+            d("Input").Should().Be("Input");
+
+            MyDelegate e = new MyDelegate(Print);
+            e("Input").Should().Be("Input");
+            e.Invoke("Input").Should().Be("Input");
+
+            MyDelegate dnull = null;
+            dnull?.Invoke("Input").Should().Be("Input");
+        }
+
+        private static string Print(string t)
+        {
+            return t;
+        }
+
+        [Test]
+        public void DelegateAnonymousMethodTest()
+        {
+            MyDelegate d = delegate (string s)
+            {
+                return s;
+            };
+            d("Anonymous Method").Should().Be("Anonymous Method");
+        }
+
+
+        [Test]
+        public void DelegateLambdaTest()
+        {
+            MyDelegate d = (string s) =>
+            {
+                return s;
+            };
+
+            MyDelegate d2 = (string s) => s;
+            
+            d("Expression lambda").Should().Be("Expression lambda");
+            d2("Statememnt lambda").Should().Be("Statememnt lambda");
+        }
+
+        [Test]
+        public void MulticastDelegateReturnValueTest()
+        {
+            MyMathDelegate doubleMe = (int num) =>
+            {
+                return num * 2;
+            };
+
+            MyMathDelegate powMe = (int num) => (int)Math.Pow(num,2);
+
+            MyMathDelegate myDoubleMe = doubleMe;
+            myDoubleMe(4).Should().Be(8);
+
+            MyMathDelegate myPowMe = powMe;
+            myPowMe(4).Should().Be(16);
+
+            MyMathDelegate myAccumulator = doubleMe;
+            myAccumulator += powMe;
+            myAccumulator(4).Should().Be(16); // not 64
+
+            MyMathDelegate myAccumulator2 = doubleMe;
+            myAccumulator2 += doubleMe;
+            myAccumulator2(4).Should().Be(8); // not 16
+
+            //acc += powMe;
+
+            //acc(2).Should().Be(16);
+
+            //acc -= doubleMe;
+
+            //acc(10).Should().Be(100);
+
+        }
+
+    }
+
+}
---
+++ CSharpPlayground/EventTests.cs
@@ -0,0 +1,120 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using System;
+
+namespace CSharpPlayground
+{
+    class EventTests
+    {
+        [Test]
+        public void PublisherSubscribersTest()
+        {
+            Subscriber s = new Subscriber();
+            Publisher p = new Publisher();
+
+            p.Added += s.AddedEventHandler;
+            p.Add(10); // AddEvent occurred
+            s.LastIndex.Should().Be(0);
+            p.Add(20); // AddEvent occurred
+            s.LastIndex.Should().Be(1);
+        }
+
+        [Test]
+        public void PublisherMultiSubscribersTest()
+        {
+            Subscriber s = new Subscriber();
+            Subscriber s2 = new Subscriber();
+            Publisher p = new Publisher();
+
+            p.Added += s.AddedEventHandler;
+            p.Added += s2.AddedEventHandler;
+            p.Add(10); // AddEvent occurred
+            s.LastIndex.Should().Be(0);
+            s2.LastIndex.Should().Be(0);
+            p.Add(20); // AddEvent occurred
+            s.LastIndex.Should().Be(1);
+            s2.LastIndex.Should().Be(1);
+        }
+
+        [Test]
+        public void PublisherSubscriberClearedTest()
+        {
+            Subscriber s = new Subscriber();
+            Publisher p = new Publisher();
+
+            p.Added += s.AddedEventHandler;
+            p.Cleared += s.ClearedEventHandler;
+            p.Add(10); // AddEvent occurred
+            p.Add(10); // AddEvent occurred
+            p.Add(10); // AddEvent occurred
+            s.LastIndex.Should().Be(2);
+
+            p.Clear(); // Cleared occurred
+            s.LastIndex.Should().Be(0);
+        }
+
+    }
+    public class CustomEventArgs : EventArgs
+    {
+        public CustomEventArgs(int index)
+        {
+            Index = index;
+        }
+
+        public int Index { get; set; }
+    }
+    class Publisher : System.Collections.ArrayList
+    {
+
+        public delegate void EventHandlerDelegate(object sender, CustomEventArgs e);
+        public event EventHandlerDelegate Added;
+
+        public event EventHandler Cleared;
+
+        protected virtual void OnAdded(CustomEventArgs e)
+        {
+            if (Added != null) Added(this, e);
+        }
+
+        public override int Add(object value)
+        {
+            int i = base.Add(value);
+            OnAdded(new CustomEventArgs(i)); // System.EventArgs.Empty
+            return i;
+        }
+
+        public override void Clear()
+        {
+            base.Clear();
+            if (Cleared != null)
+                Cleared(this, EventArgs.Empty);
+        }
+    }
+
+    class Subscriber
+    {
+        public int? LastIndex;
+        public void AddedEventHandler(object sender, CustomEventArgs e)
+        {
+            LastIndex = e.Index;
+        }
+
+        public void ClearedEventHandler(object sender, EventArgs e)
+        {
+            LastIndex = 0;
+        }
+    }
+    /*
+    class MyApp
+    {
+        static void Main()
+        {
+            Subscriber s = new Subscriber();
+            Publisher p = new Publisher();
+
+            p.Added += s.AddedEventHandler;
+            p.Add(10); // AddEvent occurred
+        }
+    }
+    */
+}
